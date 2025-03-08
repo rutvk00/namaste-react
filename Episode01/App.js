17fbc24aa95f888886ba246806a1bd4b48cf9146
rutvk00 <prajapatirutvik107@gmail.com>
2025-03-08 10:47:37 +0530

    Episode-01

---
+++ Episode01/App.js
@@ -0,0 +1,39 @@
+// ReactElement is Object which convert letter in HTML 
+
+/* 
+<div id="parent">
+    <div id="child">
+        <h1>Nested H1 Tag!</h1>
+    </div>
+</div> 
+*/
+
+const parent = React.createElement("div" , {id : "parent"} ,
+    [
+        React.createElement("div" , {id : "child1"} , 
+        [React.createElement("h1" , {} , "Nested H1 Tag!"),
+            React.createElement("h2" , {} , "Nested H2 Tag! (Sibling of H1 Tag)")
+        ]),
+        React.createElement("div" , {id : "child2"} , 
+            [React.createElement("h1" , {} , "Nested H1 Tag!"),
+                React.createElement("h2" , {} , "Nested H2 Tag! (Sibling of H1 Tag)")
+            ])
+    ]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
+
+
+
+// const heading = React.createElement("h1" , {id : "heading"} , "Hello World From React!");
+// console.log(heading);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+
+/*
+ Library - lite weight , we can use it for part of application , not compulsory to create application using this. (React)
+ Framework - heavy weight package ,  we have to create application using the framework's rules and regulation. we have to use framework's standard for developing application. (Next Js)
+
+*/
