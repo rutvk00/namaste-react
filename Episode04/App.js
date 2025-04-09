3eeff2e7929f4a557b210f90e7043241d0de3e60
rutvk00 <prajapatirutvik107@gmail.com>
2025-04-10 00:45:20 +0530

    Understand passing props to components and looping them

--- Episode04/App.js
+++ Episode04/App.js
@@ -42,18 +42,67 @@ const Header = () => {
     );
 };
 
-const RestaurantCard = () => {
+const resList = [
+    {   
+        id : 1,
+        resName : "La Crest",
+        cuisines : "Asian, Chainesse , Japaneese",
+        avgRating : "4.0 stars",
+        deliveryTime : "40 minutes",
+        imgUrl : "https://b.zmtcdn.com/data/pictures/5/20957845/23b9d70ae5086fd1587a656006597257.jpg?fit=around|750:500&crop=750:500;*,*"
+    },
+    {
+        id : 2,
+        resName : "KFC",
+        cuisines : "Burger, Chicken , Fastfood",
+        avgRating : "4.0 stars",
+        deliveryTime : "40 minutes",
+        imgUrl : "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_366/RX_THUMBNAIL/IMAGES/VENDOR/2024/12/9/568513ec-4eff-4d5f-b821-d9256da2919c_37941.JPG"
+    },
+    {
+        id : 3,
+        resName : "RP's Pizzeria",
+        cuisines : "Pizza, Garlic Bread, Fries",
+        avgRating : "4.0 stars",
+        deliveryTime : "40 minutes",
+        imgUrl : "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?resize=768,574"
+    } 
+];
+const resData = {
+    resName : "La-Crest",
+    cuisines : "Asian, Chainesse , Japaneese",
+    avgRating : "4.0 stars",
+    deliveryTime : "40 minutes",
+     imgUrl : "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?resize=768,574"
+} 
+
+const resData1 = {
+    resName : "KFC",
+    cuisines : "Burger, Chicken , Fastfood",
+    avgRating : "4.0 stars",
+    deliveryTime : "40 minutes",
+    imgUrl : "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?resize=768,574"
+} 
+
+const RestaurantCard = (props) => {
+    const {resData} = props;
+    const {
+        resName,
+        cuisines,
+        avgRating,
+        deliveryTime
+    } = resData;
     return (
         <div className='res-card' style={{backgroundColor : "#f0f0f0"}}>
             <img
                 className='res-logo'
                 alt='res-logo'
-                src='https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?resize=768,574'
+                src= {resData.imgUrl}
             />
-            <h3>La crest</h3>
-            <h4>Pasta , Western Cuisine</h4>
-            <h4>4.4 Stars</h4>
-            <h4>38 minutes</h4>
+            <h3>{resName}</h3>
+            <h4>{cuisines}</h4>
+            <h4>{avgRating}</h4>
+            <h4>{deliveryTime}</h4>
         </div>
     )
 };
@@ -63,12 +112,10 @@ const Body = () => {
         <div className='body'>
             <div className='search'>Search</div>
             <div className='res-containers'>
-               <RestaurantCard />
-               <RestaurantCard />
-               <RestaurantCard />
-               <RestaurantCard />
-               <RestaurantCard />
-               <RestaurantCard />
+               {
+                resList.map(restaurant => 
+                (<RestaurantCard key = {restaurant.id} resData={restaurant}/>))
+               }
             </div>
         </div>
     )
