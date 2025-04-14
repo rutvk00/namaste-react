a699e88f7083f4b93b92adcbc9ccfd2b054ecb54
rutvk00 <prajapatirutvik107@gmail.com>
2025-04-14 11:44:13 +0530

    Ubderstand useState hook for changing const variable

--- Episode06/src/components/Body.js
+++ Episode06/src/components/Body.js
@@ -1,13 +1,13 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
 import { SWIGGY_URL } from "../utils/constants";
+import Shimmer from "./Shimmer";
 
 import { useState, useEffect } from "react";
 
 
 const Body = () => {
 
-    const [listOfRestaurants , setListOfRestaurants] = useState(resList); 
+    const [listOfRestaurants , setListOfRestaurants] = useState([]); 
     useEffect(() =>{
         fetchData();
     } , []);
@@ -16,17 +16,21 @@ const Body = () => {
         const data = await fetch(SWIGGY_URL);
         const json = await data.json();
         // console.log(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
-        setListOfRestaurants(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants)
+        setListOfRestaurants(json?.data.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
     }
 
-    return (
+    // if(listOfRestaurants.length == 0){
+    //     return <Shimmer />
+    // }
+
+    return listOfRestaurants.length == 0 ?  <Shimmer /> : (
         <div className='body'>
             <div className='filter'>
                 <button
                  className="filter-btn"
                  onClick={() => {
                     const filteredList = listOfRestaurants.filter(
-                        (res) => res.info.avgRating >= 4
+                        (res) => res?.info?.avgRating >= 4
                     );
                     // console.log("filteredList : " , filteredList)
                     setListOfRestaurants(filteredList);
